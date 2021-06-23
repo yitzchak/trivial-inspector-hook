443011410f3bbf0da5449691689418091f7b0484
Tarn W. Burton <twburton@gmail.com>
2021-06-23 10:21:38 -0400

    Some tweaks

--- lisp/interface.lisp
+++ lisp/interface.lisp
@@ -2,7 +2,7 @@
 
 
 (defparameter *inspector-hook* nil
-"When the value of *INSPECTOR-HOOK* is non-NIL, an invocation of
+                               "When the value of *INSPECTOR-HOOK* is non-NIL, an invocation of
 INSPECT in the dynamic extent of this value results in calling the
 value with the object that was originally passed to INSPECT.  The
 function INSPECT delegates its work to the value of *INSPECTOR-HOOK*
@@ -17,35 +17,32 @@ The return value of INSPECT is independent of the return value of the
 denoted function, and remains implementation-dependent.")
 
 
-(defun initialize ()
-  (pushnew :inspector-hook *features*)
-  (pushnew :cdr-6 *features*))
-
-
 ; Support implementations that have an existing hook.
-#+(or abcl ccl clasp ecl)
+#+(or abcl ccl clasp ecl sbcl)
 (let ((sym (find-symbol #+(or abcl clasp ecl) "*INSPECTOR-HOOK*"
                         #+ccl "*DEFAULT-INSPECTOR-UI-CREATION-FUNCTION*"
                         #+mezzano "*INSPECT-HOOK*"
+                        #+sbcl "*INSPECT-FUN*"
                         ; The package
                         #+(or abcl clasp ecl) "EXT"
                         #+ccl "INSPECTOR"
-                        #+mezzano "MEZZANO.EXTENSIONS")))
+                        #+mezzano "MEZZANO.EXTENSIONS"
+                        #+sbcl "SB-IMPL")))
   (when sym
-    (shadowing-import sym "TRIVIAL-INSPECTOR-HOOK")
-    (export (find-symbol "*INSPECTOR-HOOK*" "TRIVIAL-INSPECTOR-HOOK"))
-    (initialize)))
-
-
-; Support implementations that have a hook but the input and output are also passed.
-#+sbcl
-(let ((sym (find-symbol "*INSPECT-FUN*"
-                        "SB-IMPL")))
-  (when sym
-    (setf (symbol-value sym) (lambda (object input output)
-                               (when *inspector-hook*
-                                 (let ((*standard-input* input)
-                                       (*standard-output* output))
-                                   (funcall *inspector-hook* object)))))
-    (initialize)))
-
+    (pushnew :inspector-hook *features*)
+    (pushnew :cdr-6 *features*)
+    ; SBCL's hook function has a different signature
+    #+sbcl
+    (setf (symbol-value sym)
+          (lambda (object input output)
+            (when *inspector-hook*
+              (let ((*standard-input* input)
+                    (*standard-output* output))
+                (funcall *inspector-hook* object)))))
+    #-sbcl
+    (progn
+      ; Transfer documentation. Mostly for CLASP and ECL.
+      (setf (documentation sym 'variable)
+            (documentation '*inspector-hook* 'variable))
+      (shadowing-import sym "TRIVIAL-INSPECTOR-HOOK")
+      (export (find-symbol "*INSPECTOR-HOOK*" "TRIVIAL-INSPECTOR-HOOK")))))
